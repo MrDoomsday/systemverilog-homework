df83aab3a759d669c15975c621a0e65704c782c8
RB <belenkov_rn@kursksu.ru>
2023-11-15 16:35:07 +0300

    FSM_lesson_part2
    - complete sqrt labs

--- 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_1_impl_2_fsm.sv
+++ 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_1_impl_2_fsm.sv
@@ -36,5 +36,72 @@ module formula_1_impl_2_fsm
     //
     // Design the FSM to calculate an answer and provide the correct `res` value
 
+// declaration
+    enum bit [1:0] 
+    {
+        st_idle,
+        st_wait_ab_res,
+        st_wait_c_res
+    } state, state_next;
+
+bit [31:0] c_fix;//временно храним переменную C 
+bit [31:0] sum_sqrt_a_plus_b;//хранение промежуточного результата
+
+// logic
+always_ff @ (posedge clk or posedge rst)
+    if(rst) state <= st_idle;
+    else state <= state_next;
+
+
+always_comb begin
+    state_next = state;
+    isqrt_1_x_vld = 1'b0;
+    isqrt_1_x = a;
+    isqrt_2_x_vld = 1'b0;
+    isqrt_2_x = b;
+
+    case(state)
+        st_idle: begin
+            if(arg_vld) begin
+                isqrt_1_x_vld = 1'b1;
+                isqrt_2_x_vld = 1'b1;
+                state_next = st_wait_ab_res;
+            end
+        end
+
+        st_wait_ab_res: begin
+            if(isqrt_1_y_vld && isqrt_2_y_vld) begin//задержки на вычисление одинаковые, в случае разных задержек вычисления (либо плавающих) пришлось бы добавить еще несколько состояний.
+                isqrt_1_x = c_fix;
+                isqrt_1_x_vld = 1'b1;
+                state_next = st_wait_c_res;
+            end
+        end
+
+        st_wait_c_res: begin
+            if(isqrt_1_y_vld) begin
+                state_next = st_idle;
+            end
+        end
+
+        default: state_next = st_idle;
+    endcase
+end
+
+
+
+always_ff @ (posedge clk) begin
+    if((state == st_idle) && arg_vld) c_fix <= c;
+    if((state == st_wait_ab_res) && isqrt_1_y_vld && isqrt_2_y_vld) sum_sqrt_a_plus_b <= {16'h0, isqrt_1_y} + {16'h0, isqrt_2_y};//extend to 32-bit number
+    if((state == st_wait_c_res) && isqrt_1_y_vld) res <= sum_sqrt_a_plus_b + {16'h0, isqrt_1_y};
+end
+
+
+always_ff @ (posedge clk or posedge rst)
+    if(rst) res_vld <= 1'b0;
+    else res_vld <= (state == st_wait_c_res) & isqrt_1_y_vld;
+
+
+
+
 
 endmodule
--- 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_2_fsm.sv
+++ 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_2_fsm.sv
@@ -28,6 +28,77 @@ module formula_2_fsm
     // using only one instance of the isrt module.
     //
     // Design the FSM to calculate answer step-by-step and provide the correct `res` value
+enum bit [1:0] 
+{
+    st_idle,
+    st_wait_sqrt_1,//sqrt(c)
+    st_wait_sqrt_2,//sqrt(b+sqrt(c))
+    st_wait_sqrt_3//sqrt(a + sqrt(b + sqrt(c)))
+} state, state_next;
+
+bit [31:0] a_fix, b_fix;
+
+
+always_ff @ (posedge clk or posedge rst)
+    if(rst) state <= st_idle;
+    else state <= state_next;
+
+always_comb begin
+   state_next = state;
+   isqrt_x = c;
+   isqrt_x_vld = 1'b0;
+
+   case(state)
+    st_idle: begin
+        if(arg_vld) begin
+            isqrt_x_vld = 1'b1;
+            state_next = st_wait_sqrt_1;
+        end
+    end
+
+    st_wait_sqrt_1: begin
+        if(isqrt_y_vld) begin
+            isqrt_x_vld = 1'b1;
+            isqrt_x = {16'h0, isqrt_y} + b_fix;
+            state_next = st_wait_sqrt_2;
+        end
+    end
+
+    st_wait_sqrt_2: begin
+        if(isqrt_y_vld) begin
+            isqrt_x_vld = 1'b1;
+            isqrt_x = {16'h0, isqrt_y} + a_fix;
+            state_next = st_wait_sqrt_3;
+        end
+    end
+
+    st_wait_sqrt_3: begin
+        if(isqrt_y_vld) begin
+            state_next = st_idle;
+        end
+    end
+
+    default: state_next = st_idle;
+   endcase
+end
+
+always_ff @ (posedge clk) begin
+    if((state == st_idle) && arg_vld) begin
+        a_fix <= a;
+        b_fix = b;
+    end
+
+end
+
+always_ff @ (posedge clk or posedge rst)
+    if(rst) begin
+        res_vld <= 1'b0;
+        res <= 32'h0;
+    end
+    else begin
+        res_vld <= (state == st_wait_sqrt_3) & isqrt_y_vld;
+        res <= {16'h0, isqrt_y};
+    end
 
 
 endmodule
